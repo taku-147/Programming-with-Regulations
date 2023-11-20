88424b8c443f2d432aaea74815b2c59002da1591
taku-147 <44944463+taku-147@users.noreply.github.com>
2023-11-20 14:08:48 +0900

    Ver1.0
    一応制約には則ったつもりです。
    おそらく。多分。うん。

---
+++ source.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+void math(unsigned int a, unsigned int b) {
+    fprintf(stdout, "a b? >");
+    scanf("%d %d", &a, &b);
+    fprintf(stdout, "%d + %d = %d\n", a, b, a + b);
+    fprintf(stdout, "%d - %d = %d\n", a, b, a - b);
+    fprintf(stdout, "%d * %d = %d\n", a, b, a * b);
+    fprintf(stdout, "%d / %d = %d...%d\n", a, b, a / b, a % b);
+}
+int main(void)
+{
+    math(NULL,NULL);
+    return 0;
+}
