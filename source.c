efcebda0829d2e6175c93577ff2857a52e50b6f2
taku-147 <44944463+taku-147@users.noreply.github.com>
2023-11-20 14:24:55 +0900

    Ver1.1
    四則演算記号選択を追加

--- source.c
+++ source.c
@@ -1,11 +1,28 @@
+
 #include <stdio.h>
 void math(unsigned int a, unsigned int b) {
-    fprintf(stdout, "a b? >");
-    scanf("%d %d", &a, &b);
-    fprintf(stdout, "%d + %d = %d\n", a, b, a + b);
-    fprintf(stdout, "%d - %d = %d\n", a, b, a - b);
-    fprintf(stdout, "%d * %d = %d\n", a, b, a * b);
-    fprintf(stdout, "%d / %d = %d...%d\n", a, b, a / b, a % b);
+    fprintf(stdout, "数字(a)を入力:");
+    scanf("%d", &a);
+    fprintf(stdout, "数字(b)を入力:");
+    scanf("%d", &b);
+    getchar();
+    fprintf(stdout, "四則演算記号(+|-|*|/)を入力:");
+    switch (getchar()) {
+    case '+':
+        fprintf(stdout, "%d + %d = %d\n", a, b, a + b);
+        break;
+    case '-':
+        fprintf(stdout, "%d - %d = %d\n", a, b, a - b);
+        break;
+    case '*':
+        fprintf(stdout, "%d * %d = %d\n", a, b, a * b);
+        break;
+    case '/':
+        fprintf(stdout, "%d / %d = %d...%d\n", a, b, a / b, a % b);
+        break;
+    default:
+        fprintf(stdout, "+|-|*|/から選択してください。\n");
+    }
 }
 int main(void)
 {
